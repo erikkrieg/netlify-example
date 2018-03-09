0163fcb23ffcfacc824f1d6de2b5ac7a60de5f43
Erik Krieg <erik.c.krieg@gmail.com>
2018-03-08 19:58:34 -0500

    configures gatsby to transform markdown into pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: ['gatsby-plugin-netlify-cms'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-netlify-cms', // The Netlify plugin should be last in the array
+  ],
 };
