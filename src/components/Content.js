18e111a72208bc22219e382816eb8e2cd6881772
Erik Krieg <erik.c.krieg@gmail.com>
2018-03-08 21:00:34 -0500

    adds Content component that can render html strings or nodes

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default { content } => (
+  <Fragment>
+    {typeof content === 'string' ? (
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    ) : (
+      <div>{content}</div>
+    )}
+  </Fragment>
+)
