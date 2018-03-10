2c76138296121dd0768e88ae03a9ca798f26af29
Erik Krieg <erik.c.krieg@gmail.com>
2018-03-10 14:40:39 -0500

    updates index page to hide links to student and teacher pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ export default () => (
       <h1>Prodigy Support 💖</h1>
     </header>
     <ul>
-      <li><a href="/topics/teachers">Teachers</a></li>
+      {/* <li><a href="/topics/teachers">Teachers</a></li> */}
       <li><a href="/topics/parents">Parents</a></li>
-      <li><a href="/topics/students">Students</a></li>
+      {/* <li><a href="/topics/students">Students</a></li> */}
     </ul>
   </main>
 )
