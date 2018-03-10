96d84692302fc251b03b4481af7824c692eb7687
Erik Krieg <erik.c.krieg@gmail.com>
2018-03-10 12:19:18 -0500

    updates index page to link to high level topics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default () => (
-  <div>Hello world!</div>
+  <main>
+    <header>
+      <h1>Prodigy Support 💖</h1>
+    </header>
+    <ul>
+      <li><a href="/topics/teachers">Teachers</a></li>
+      <li><a href="/topics/parents">Parents</a></li>
+      <li><a href="/topics/students">Students</a></li>
+    </ul>
+  </main>
 )
