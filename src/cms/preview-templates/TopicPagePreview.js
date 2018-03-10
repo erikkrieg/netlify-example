e28b512de7728b59050743ba9066a67473e4cd6e
Erik Krieg <erik.c.krieg@gmail.com>
2018-03-10 13:35:04 -0500

    updates topic page preview to display topics

--- src/cms/preview-templates/TopicPagePreview.js
+++ src/cms/preview-templates/TopicPagePreview.js
@@ -4,6 +4,7 @@ import { TopicPageTemplate } from '../../templates/TopicPage'
 const TopicPagePreview = ({ entry, widgetFor }) => (
   <TopicPageTemplate
     title={entry.getIn(['data', 'title'])}
+    topics={entry.getIn(['data', 'topics'])}
   />
 )
 
